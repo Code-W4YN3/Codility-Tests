0701645e521c2f1f1124b80be8b5b8cb0de97919
wayne-otido <otidowayne@gmail.com>
2023-10-17 11:07:59 +0300

    removed a for loop in asphault

--- Asphault-patches/asphault.py
+++ Asphault-patches/asphault.py
@@ -1,10 +1,5 @@
 def solution(S):
-    initial_dots = 0
-    for segment in S:
-        if segment != ".":
-            break
-        initial_dots += 1
-    patchable = S[initial_dots:]
+    patchable = S[S.index('X'):]
     # print(patchable)
 
     groups = [(patchable[i:i+3]) for i in range(0, len(patchable), 3)]
@@ -16,10 +11,10 @@ def solution(S):
     for i in groups:
         if "X" in i:
             count += 1
-    
+    # print(count)
     return count
 
-# solution('X.XXXXX.X.')
-# solution('XX.XXX..')
-# solution('XXXX')
-# solution('.X...XX')
+solution('X.XXXXX.X.')
+solution('XX.XXX..')
+solution('XXXX')
+solution('.X...XX')
